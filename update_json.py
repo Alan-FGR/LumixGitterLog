18c66f12b1313f6bfedb3eda90821211771c9fa6
Alan Q <alanqdev@gmail.com>
2017-10-17 13:53:53 -0200

    no message

--- update_json.py
+++ update_json.py
@@ -7,6 +7,7 @@ data = []
 users = []
 
 sucks = 0
+sucksdict = {}
 
 for key in hist:
     log = hist[key]
@@ -18,8 +19,20 @@ for key in hist:
     if usr not in users:
         users.append(usr)
 
-    sucks+=msg.count("suck")
-    sucks+=msg.count("sux")
+
+#suckylogic
+
+    if usr not in sucksdict:
+        sucksdict[usr] = 0;
+
+
+    sucksinmsg = msg.count("sux")+msg.count("suck");
+    sucksdict[usr] += sucksinmsg;
+    sucks += sucksinmsg;
+
+
+
+
 
     # TODO UNCOMMENT WHEN YOU CAN MAKE LAZY LOADER WORK :(
     # if ("<img" in log['html']):
@@ -33,7 +46,7 @@ for key in hist:
         'html': msg
     })
 
-open("log.json", 'w').write(json.dumps(data))  # , indent=2))
+#open("log.json", 'w').write(json.dumps(data))  # , indent=2))
 
 users.sort()
 users.insert(0,"All")
@@ -41,9 +54,24 @@ users_str = ""
 for u in users:
     users_str+='<option value="{}">{}</option>\n'.format(u,u)
 
+
+
+sucks_scores = sorted(sucksdict.items(), key=lambda x: x[1], reverse=True);
+sucksstr = "TOTAL SUCKS GIVEN: "+str(sucks)
+sucksstr += "<br><br>TOP SCORES:"
+sucksstr += "<table class=\"sux\">"
+c=0;
+for entry in sucks_scores:
+    sucksstr += "<tr><td>"+entry[0]+"</td><td>"+str(entry[1])+"</td></tr>"
+    c+=1;
+    if c>=8:
+        break
+sucksstr += "</table>"
+
 open("index.html", 'w').write(open("template.html", 'r').read()
                               .replace("[SHITGOESHERE]", 'Messages: {}, Users: {}, Updated: {}'.format(len(data),len(users),str(date.today())))
                               .replace("[USERSGOESHERE]", users_str)
-                              .replace("[SUCKSCOUNTER]", str(sucks))
+                              .replace("[SUCKSCOUNTER]", sucksstr)
                               .replace("[SIZEGOESHERE]", '{:.2f}'.format((len(json.dumps(data).encode())/1000000)/4.75)) #approx zip compression
                               )
+
